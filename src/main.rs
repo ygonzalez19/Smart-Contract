f8a8eeea8ea89b2c1e6b33e2b0b0971db35733d0
steven <corderosteven6@gmail.com>
2025-01-23 21:42:22 -0600

    feat: create dotenv file and serde

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use axum::{Router, routing::get};
-use std::net::SocketAddr;
+use crate::config::Config;
+use axum::{routing::get, Router};
+use dotenvy::dotenv;
+use std::{env, net::SocketAddr};
 
+mod config;
 mod routes;
+
+fn load_env() {
+    dotenv().ok();
+    let project_id = env::var("FIREBASE_PROJECT_ID").expect("FIREBASE_PROJECT_ID not set");
+    println!("Firebase Project ID: {}", project_id);
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    let app = Router::new()
-        .route("/health", get(routes::health::health_check));
+    load_env();
+
+    let config = Config::from_env();
+
+    log::info!("Firebase Project ID: {}", config.firebase_project_id);
+    log::info!("Firebase Client Email: {}", config.firebase_client_email);
+
+    let app = Router::new().route("/health", get(routes::health::health_check));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     log::info!("Running on http://{}", addr);
